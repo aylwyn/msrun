c9f1df74a7b13ebe060d4a2ca1a360d099b62d4d
aylwyn <aylwyn@cantab.net>
2015-04-18 17:49:49 +0100

    Add pop growth --eG --eg --eNG --eng; change arg format

--- msrun.py
+++ msrun.py
@@ -16,23 +16,30 @@ import string
 import decimal
 import argparse
 
-from aosutils import *
+import aosutils
 
-p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-p.add_argument('-s', '--nsamps', type = int, default = 4, help = 'number of samples per rep')
-p.add_argument('-n', '--nreps', type = int, default = 1, help = 'number of repetitions')
-p.add_argument('-l', '--seqlen', type = int, default = 10000, help = 'sequence length simulated')
-p.add_argument('-p', '--npops', type = int, default = 1, help = 'number of populations')
-p.add_argument('-u', '--mugen', type = float, default = 1.25e-8, help = 'per-generation mutation rate')
-p.add_argument('-N', '--N0', type = int, default = 10000, help = 'base effective population size')
+#p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+p = argparse.ArgumentParser()
+p.add_argument('-s', '--nsamps', type = int, default = 4, help = 'number of samples per rep (default = 4)')
+p.add_argument('-n', '--nreps', type = int, default = 1, help = 'number of repetitions (default = 1)')
+p.add_argument('-l', '--seqlen', type = int, default = 10000, help = 'sequence length simulated (default = 10000)')
+p.add_argument('-p', '--npops', type = int, default = 1, help = 'number of populations (default = 1)')
+p.add_argument('-u', '--mugen', type = float, default = 1.25e-8, help = 'per-generation mutation rate (default = 1.25e-8)')
+p.add_argument('-N', '--N0', type = int, default = 10000, help = 'base effective population size (default = 10000)')
 p.add_argument('-r', '--recgen', type = float, default = 0.0, help = 'per-generation recombination rate')
-p.add_argument('-g', '--tgen', type = float, default = 30.0, help = 'generation time (y)')
-p.add_argument('-T', '--trees', action='store_true', default = False, help = 'inlude trees in ms output')
-p.add_argument('--mrca', action='store_true', default = False, help = 'inlude TMRCA in ms output')
-p.add_argument('--eN', help='global Ne history: "time,Ne ..."' )
-p.add_argument('--en', help='population Ne history: "time,pop,Ne ..."' )
-p.add_argument('--ej', help='population merge_history: "time,from_pop,to_pop ..."')
-p.add_argument('--em', help='migration_history: "time,to_pop,from_pop,to_pop_mig_frac ..."')
+p.add_argument('-g', '--tgen', type = float, default = 30.0, help = 'generation time (y) (default = 30.0)')
+p.add_argument('-T', '--trees', action='store_true', default = False, help = 'include trees in ms output')
+p.add_argument('--mrca', action='store_true', default = False, help = 'include TMRCA in ms output')
+
+p.add_argument('--eN', action='append', default = [], nargs = 2, help='global Ne history', metavar = ('time', 'Ne'))
+p.add_argument('--eG', action='append', default = [], nargs = 2, help='global growth history', metavar = ('time', 'alpha'))
+p.add_argument('--eNG', action='append', default = [], nargs = 4, help='global growth history in terms of time intervals and Ne change (Note: t2 is older than t1)', metavar = ('time1', 'time2', 'Ne1', 'Ne2'))
+p.add_argument('--en', action='append', default = [], nargs = 3, help='population Ne history', metavar = ('time', 'pop', 'Ne'))
+p.add_argument('--eg', action='append', default = [], nargs = 3, help='population growth history', metavar = ('time', 'pop', 'alpha'))
+p.add_argument('--eng', action='append', default = [], nargs = 5, help='population growth history in terms of time intervals and Ne change (Note: t2 is older than t1)', metavar = ('pop', 'time1', 'time2', 'Ne1', 'Ne2'))
+p.add_argument('--ej', action='append', default = [], nargs = 3, help='population merge_history', metavar = ('time', 'from_pop', 'to_pop'))
+p.add_argument('--em', action='append', default = [], nargs = 4, help='migration_history', metavar = ('time', 'to_pop', 'from_pop', 'to_pop_mig_frac'))
+
 p.add_argument('--macs', action='store_true', default = False, help = 'output MaCS command (otherwise use ms)')
 p.add_argument('--scrm', action='store_true', default = False, help = 'output scrm command (otherwise use ms)')
 p.add_argument('--recfile', help='MaCS-formatted recombination rate file (enforces --macs, ignores -r)' )
@@ -42,9 +49,9 @@ p.add_argument('--msr', type = float, help = 'ms rho value; overrides --recgen i
 p.add_argument('--msargs', help = 'ms arguments: "nsamps nreps -t mst [-r msr seqlen] [-I npops pop1_nsamps [pop2_nsamps ...]] <ms_options>" (see MS documentation for more options)')
 p.add_argument('--macsargs', help = 'MaCS arguments: "nsamps seqlen -i nreps -t macst [-r macsr] [-I npops pop1_nsamps [pop2_nsamps ...]] <macs_options>" (see MaCS documentation for more options)')
 p.add_argument('--scrmargs', help = 'scrm arguments: "nsamps nreps -t mst [-r msr seqlen] [-I npops pop1_nsamps [pop2_nsamps ...]] <scrm_options>" (see scrm documentation for more options)')
-p.add_argument('--outfile', action='store_true', default = False, help = 'redirect output to OUTFILE')
-p.add_argument('--prefix', default = 'sim', help='output prefix')
-p.add_argument('--suffix', default='ms', help='output suffix')
+p.add_argument('--outfile', action='store_true', default = False, help = 'redirect output to file')
+p.add_argument('--prefix', default = 'mssim', help='output file prefix')
+p.add_argument('--suffix', default='', help='output file suffix')
 p.add_argument('--encode_pars', action='store_true', default = False, help = 'encode parameters in output file name')
 p.add_argument('--unscale_string', help='print unscaled parameters for simulation output name UNSCALE_STRING')
 p.add_argument('--bsub', action='store_true', default = False, help = 'output submit.py bsub command')
@@ -52,6 +59,7 @@ p.add_argument('--batch', action='store_true', default = False, help = 'output s
 p.add_argument('--zipout', action='store_true', default = False, help = 'add zipout flag to submit.py call')
 p.add_argument('-P', '--pipecmds', help = 'pipe commands')
 p.add_argument('-a', '--allargs', action='store_true', default = False, help = 'encode all arguments in output name')
+p.add_argument('--sigfigs', type=int, default = 4, help = 'sig figs to use for numbers')
 p.add_argument('--run', action='store_true', default = False, help = 'run ms command')
 p.add_argument('--sim', action='store_true', default = False, help = 'dry run')
 p.add_argument('-v', '--verbose', action='store_true', default = False)#, help = 'dry run')
@@ -60,6 +68,8 @@ p.add_argument('--debug', action='store_true', default = False, help=argparse.SU
 #pp.add_argument('--replace', action='store_true', default = False, help = 'replace existing files')
 
 args = p.parse_args()
+
+def fnum(x): return aosutils.fnum(x, args.sigfigs)
 
 loglevel = logging.WARNING
 if args.verbose:
@@ -103,6 +113,7 @@ if args.recfile or args.chrmap:
 		recfile.close()
 
 encmd = []
+encmd_t = [] # time-ordered commands
 if args.macs and args.nreps:
 	encmd.append('-i %d' % (args.nreps))
 if not args.mst:
@@ -131,37 +142,67 @@ if args.trees:
 	encmd.append('-T')
 if args.mrca:
 	encmd.append('-L')
-if args.eN:
-	for x in args.eN.split():
-	#	print(x)
-		tev = eval(x.split(',')[0]) / (4 * args.N0 * args.tgen)
-		Nev = float(eval(x.split(',')[1])) / args.N0
-		encmd.append('-eN %s %s' % (fnum(tev), fnum(Nev)))
-if args.en:
-	for x in args.en.split():
-		tev = eval(x.split(',')[0]) / (4 * args.N0 * args.tgen)
-		pnum = int(x.split(',')[1])
-		Nev = float(eval(x.split(',')[2])) / args.N0
-		encmd.append('-en %s %d %s' % (fnum(tev), pnum, fnum(Nev)))
-if args.ej:
-	for x in args.ej.split():
-		tev = eval(x.split(',')[0]) / (4 * args.N0 * args.tgen)
-		p1, p2 = x.split(',')[1:]
-		encmd.append('-ej %s %s %s' % (fnum(tev), p1, p2))
-if args.em:
-	for x in args.em.split():
-		tev = eval(x.split(',')[0]) / (4 * args.N0 * args.tgen)
-		p1, p2 = x.split(',')[1:3]
-		mig = eval(x.split(',')[3]) * 4 * args.N0
-		encmd.append('-em %s %s %s %s' % (fnum(tev), p1, p2, fnum(mig)))
+
+for x in args.eN:
+##	print(x)
+#	tev = eval(x.split(',')[0]) / (4 * args.N0 * args.tgen)
+#	Nev = float(eval(x.split(',')[1])) / args.N0
+	tev = eval(x[0]) / (4 * args.N0 * args.tgen)
+	Nev = float(eval(x[1])) / args.N0
+	encmd_t.append((tev, '-eN %s %s' % (fnum(tev), fnum(Nev))))
+for x in args.eG:
+	tev = eval(x[0]) / (4 * args.N0 * args.tgen)
+	alpha = float(eval(x[1]))
+	encmd_t.append((tev, '-eG %s %s' % (fnum(tev), fnum(alpha))))
+for x in args.eNG:
+	tev = eval(x[0]) / (4 * args.N0 * args.tgen)
+	tev2 = eval(x[1]) / (4 * args.N0 * args.tgen)
+	Nev1 = float(eval(x[2])) / args.N0
+	Nev2 = float(eval(x[3])) / args.N0
+	alpha = -(1 / (tev2 - tev)) * ln(Nev2 / Nev1)
+	encmd_t.append((tev, '-eN %s %s' % (fnum(tev), fnum(Nev1))))
+	encmd_t.append((tev, '-eG %s %s' % (fnum(tev), fnum(alpha))))
+	encmd_t.append((tev, '-eG %s 0' % (fnum(tev2))))
+for x in args.en:
+	tev = eval(x[0]) / (4 * args.N0 * args.tgen)
+	pnum = int(x[1])
+	Nev = float(eval(x[2])) / args.N0
+	encmd_t.append((tev, '-en %s %d %s' % (fnum(tev), pnum, fnum(Nev))))
+for x in args.eg:
+	tev = eval(x[0]) / (4 * args.N0 * args.tgen)
+	pnum = int(x[1])
+	alpha = float(eval(x[2]))
+	encmd_t.append((tev, '-eg %s %d %s' % (fnum(tev), pnum, fnum(alpha))))
+for x in args.eng:
+	pnum = int(x[0])
+	tev = eval(x[1]) / (4 * args.N0 * args.tgen)
+	tev2 = eval(x[2]) / (4 * args.N0 * args.tgen)
+	Nev1 = float(eval(x[3])) / args.N0
+	Nev2 = float(eval(x[4])) / args.N0
+	alpha = -(1 / (tev2 - tev)) * ln(Nev2 / Nev1)
+	encmd_t.append((tev, '-en %s %d %s' % (fnum(tev), pnum, fnum(Nev1))))
+	encmd_t.append((tev, '-eg %s %d %s' % (fnum(tev), pnum, fnum(alpha))))
+	encmd_t.append((tev, '-eg %s %d 0' % (fnum(tev2), pnum)))
+for x in args.ej:
+	tev = eval(x[0]) / (4 * args.N0 * args.tgen)
+	p1, p2 = x[1:]
+	encmd_t.append((tev, '-ej %s %s %s' % (fnum(tev), p1, p2)))
+for x in args.em:
+	tev = eval(x[0]) / (4 * args.N0 * args.tgen)
+	p1, p2 = x[1:3]
+	mig = eval(x[3]) * 4 * args.N0
+	encmd_t.append((tev, '-em %s %s %s %s' % (fnum(tev), p1, p2, fnum(mig))))
+
+encmd_t.sort()
+encmd.append(' '.join([x[1] for x in encmd_t]))
 
 if args.unscale_string:
-	if not args.unscale_string.endswith('.mssim'):
-		error('--unscale_string currently only works with .mssim files')
+	if not args.unscale_string.endswith('.ms'):
+		error('--unscale_string currently only works with .ms files')
 	print('Using mugen = %e per bp per generation, tgen = %.1f y:' % (args.mugen, args.tgen))
 	msargs = {}
 #	for tok in (x.split('_') for x in simname.split('-')[1:]):
-	for i, x in enumerate(args.unscale_string.replace('.mssim', '').split('-')):
+	for i, x in enumerate(args.unscale_string.replace('.ms', '').split('-')):
 		if i == 0 and x.isdigit():
 			print('%s samples' % x)
 			continue
@@ -223,8 +264,8 @@ else:
 
 outname = args.prefix
 if args.encode_pars:
-	if outname:
-		outname += '.'
+#	if outname:
+#		outname += '.'
 	if args.allargs or args.macs:
 		outname += '_'.join(outargs.split()).replace('_-', '-')
 	else:
